805a15bb4f67263b621d6356104a648a1dc3f26c
miziloner <mizilonerdepumas@gmail.com>
2020-06-09 17:32:03 -0500

    fds
    fds

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,6 +1,6 @@
 apt-get update
 apt-get install python3.6
-apt-get -y  python3-pip
+apt install python3-pip
 pip install pandas==0.25.1
 pip install pyarrow==0.17.0
 pip install azure-storage-blob==2.1.0
@@ -9,3 +9,4 @@ python3 -m pip install pandas==0.25.1
 python3 -m pip install pyarrow==0.17.0
 python --version 
  
+ -m pip install pip
