2158009695da8f08fe2dadee86592e3f46301eb4
miziloner <mizilonerdepumas@gmail.com>
2020-06-09 14:47:10 -0500

    okokoko
    haha

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,5 +2,5 @@ apt-get update
 apt-get -y install python-dev python-pip
 pip install pandas
 pip install pyarrow
-pip install azure-storage-blob
+pip install azure-storage-blob==2.1.0
 python --version 
