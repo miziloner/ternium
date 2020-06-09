3009bc1a6cfc80b2e8ff7e65e9e1606aafa42154
miziloner <mizilonerdepumas@gmail.com>
2020-06-09 14:27:56 -0500

    cambio storage

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,8 +1,6 @@
-set -e 
 apt-get update 
-apt-get -y install python-dev python-pip 
-pip install psutil python-dateutil applicationinsights==0.11.3 
-pip install azure-storage
+apt-get -y install python-dev python-pip
 pip install pandas==0.25.1
 pip install pyarrow==0.17.0
+pip install azure-storage-blob
 python --version 
