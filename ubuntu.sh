56a5e323d0b624631402dd6d1458c37042370519
miziloner <mizilonerdepumas@gmail.com>
2020-06-09 14:35:13 -0500

    ss
    sss

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,6 +1,6 @@
 apt-get update 
 apt-get -y install python-dev python-pip
-pip install pandas==0.25.1
-pip install pyarrow==0.17.0
+pip install pandas
+pip install pyarrow
 pip install azure-storage-blob
 python --version 
