29094bd1bdb92010449dce225f634bd3bcfb9877
miziloner <mizilonerdepumas@gmail.com>
2020-06-09 17:22:02 -0500

    fcsd
    vrwcdes

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,6 +4,8 @@ apt-get -y  python-pip
 pip install pandas==0.25.1
 pip install pyarrow==0.17.0
 pip install azure-storage-blob==2.1.0
+python3 -m pip install numpy
+python3 -m pip install pandas==0.25.1
+python3 -m pip install pyarrow==0.17.0
 python --version 
-
  
