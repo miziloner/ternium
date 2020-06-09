da65bde323981be9788f652a0ddc568a0de354ae
miziloner <mizilonerdepumas@gmail.com>
2020-06-09 17:52:34 -0500

    fcdsx
    vfcf

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,6 @@
 apt-get update
 apt-get install python3.6
+apt-get -y install python3-pip
 python3.6 -m pip install pandas==0.25.1
 python3.6 -m pip install pyarrow==0.17.0
 python3.6 -m pip install azure-storage-blob==2.1.0
