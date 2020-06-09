007ad78ac784dff1cdb8e3c991421fe5d02fe5c1
miziloner <mizilonerdepumas@gmail.com>
2020-06-09 17:06:17 -0500

    update all
    update all

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,6 +1,8 @@
 apt-get update 
 apt-get -y install python3.6 python-pip
-pip install pandas
-pip install pyarrow
+pip install pandas==0.25.1
+pip install pyarrow==0.17.0
 pip install azure-storage-blob==2.1.0
 python --version 
+
+ 
