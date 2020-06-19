0b131ec244ea04c7b4c86ce6072e2d41ebeb9471
miziloner <mizilonerdepumas@gmail.com>
2020-06-19 09:12:12 -0500

    sss
    sss

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,4 +5,5 @@ apt-get -y install python3-pip
 python3.6 -m pip install pandas==0.25.1
 python3.6 -m pip install pyarrow==0.17.0
 python3.6 -m pip install azure-storage-blob==2.1.0
-python3 --version 
+python3.6 -m pip install psutil python-dateutil
+python3 --version  
